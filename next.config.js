58bcd0d1680202f1e17ee062cb260b52dd4a77b3
bublik321 <53870458+bublik321@users.noreply.github.com>
2022-08-24 17:04:12 +0300

    312

--- next.config.js
+++ next.config.js
@@ -1,3 +1,7 @@
-const withTM = require('@vercel/examples-ui/transpile')()
 
-module.exports = withTM()
+const withNextra = require('nextra')({
+    theme: 'nextra-theme-docs',
+    themeConfig: './theme.config.js',
+    unstable_staticImage: true,
+  })
+module.exports = withNextra()
